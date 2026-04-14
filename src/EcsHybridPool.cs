c6d37a05252453b8a3e8a1a3277ec720a2759c78
Mikhail <99481254+DCFApixels@users.noreply.github.com>
2026-04-14 12:52:27 +0800

    Update EcsHybridPool.cs

--- src/EcsHybridPool.cs
+++ src/EcsHybridPool.cs
@@ -4,7 +4,7 @@
 using DCFApixels.DragonECS.Core;
 using DCFApixels.DragonECS.Hybrid;
 using DCFApixels.DragonECS.Hybrid.Internal;
-using DCFApixels.DragonECS.PoolsCore;
+using DCFApixels.DragonECS.Core;
 using System;
 using System.Collections;
 using System.Collections.Generic;
